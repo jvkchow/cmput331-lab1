24592f4a7380c14c15a90ccbede761b8442b0a8c
Jan Chow <tobelhead@gmail.com>
2023-09-13 19:20:37 -0600

    added new version of lab1.py

--- lab1.py
+++ lab1.py
@@ -2,6 +2,6 @@ import requests
 
 #print(requests.__version__)
 
-resp = requests.get("http://google.com")
+resp = requests.get("https://raw.githubusercontent.com/jvkchow/cmput404-lab1/main/lab1.py")
 
-print(resp.txt)
+print(resp.text)
